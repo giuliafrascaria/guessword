a72f3c44424c4379244c59cb5cdb7fe957fb4cf9
Giulia Frascaria <giulia.frascaria@gmail.com>
2019-09-05 18:38:17 +0200

    pthreads working

--- guessword.c
+++ guessword.c
@@ -7,6 +7,7 @@
 #include <sys/stat.h>
 #include <fcntl.h>
 #include <ctype.h>
+#include <pthread.h>
 
 
 //structs
@@ -20,14 +21,26 @@ struct user_details {
 	char * full_name;
 	char * hash;
 	int found;
+	int uid;
 	struct user_details * next;
 };
+
+struct thread_args {
+	int tid;
+	struct user_details * user_list;
+	struct pwd_hash * hash_table;
+};
+
+#define PTHREAD_N 8
+pthread_t threads[PTHREAD_N];
 
 //function definitions
 void parse_top_250(char * salt, struct pwd_hash ** array);
 char * do_pwd_hash(char *pwd, char *salt);
 char * check_name_patterns(char * name, char * hash, char * salt);
 char * test_name_year(char *name, char *salt, char *hash);
+void * crack_partition(void * arg);
+void join_threads(void);
 
 
 //let the cracking begin
@@ -58,14 +71,6 @@ int main (int argc, char ** argv)
 
 
 	//retrieve salt from shadow file
-	//char *lineptr = NULL;
-	//size_t n = 0;
-	//ssize_t len = getline(&lineptr, &n, shd);
-	//if (len < 0)
-	//{
-	//	printf("error reading shadow file for salt\n");
-	//	exit(EXIT_FAILURE);
-	//}
 	char *lineptr = malloc(32 * sizeof(char));
 	lineptr = fgets(lineptr, 32, shd);
 	if (lineptr == NULL)
@@ -73,9 +78,6 @@ int main (int argc, char ** argv)
 		printf("error reading shadow file for salt\n");
 		exit(EXIT_FAILURE);
 	}
-
-
-
 	char * salt = malloc(6*sizeof(char));
 	if (salt == NULL)
 	{
@@ -87,17 +89,20 @@ int main (int argc, char ** argv)
 	free(lineptr);
 	fseek(shd, 0, SEEK_SET);
 
-	//create username list
+
+
+	//create user list
 	struct user_details * head, *current;
 	head = NULL;
 	current = NULL;
+
+	int uid = 0;
 
 	char * pwdline = malloc(128 * sizeof(char));
 	char * shdline = malloc(64 * sizeof(char));
 	while (fgets(pwdline, 128, pwd))
 	{
 		shdline = fgets(shdline, 64, shd);
-
 
 		struct user_details * node = malloc(sizeof(struct user_details));
 		node->username = malloc(6*sizeof(char));
@@ -118,7 +123,9 @@ int main (int argc, char ** argv)
 		node->hash = memcpy(node->hash, &shdline[7], 28);
 
 		node->found = 0;
-		printf("%s:%s:%s\n", node->username, node->hash, node->full_name);
+		node->uid = uid%8;
+		uid++;
+		printf("%d:%s:%s:%s\n", node->uid, node->username, node->hash, node->full_name);
 
 		if(head == NULL){
       current = head = node;
@@ -127,11 +134,24 @@ int main (int argc, char ** argv)
     }
 
 	}
-	//return(0);
+
 
 	//read user names and test nameYYYY first, before going to threads
 	//delete found users from worklist
+	int matches = 0;
+	printf("starting to check\n");
 
+	for(current = head; current ; current=current->next)
+	{
+		//for each user check name-dependant stuff
+		char *success = check_name_patterns(current->full_name, current->hash, salt);
+		if (success != NULL)
+		{
+			printf("%s:%s\n", current->username, success);
+			current->found = 1;
+			matches++;
+		}
+  }
 
 	//open dictionaries and create all necessary data structures
 	//struct pwd_hash *pwd_hashes_250 = malloc(478 * sizeof(struct pwd_hash));
@@ -170,180 +190,56 @@ int main (int argc, char ** argv)
 		i++;
 
 	}
-
-	//for (int i = 0; i < 478; i++)
-	//{
-	//	printf("%s %s\n", (pwd_hashes_250[i]).hash, (pwd_hashes_250[i]).pwd);
-	//}
 	//continue this part
 //------------------------------------------------------------------------------------------------------------------
 
 	//check against top250 passwords
 	//len = 0;
+	printf("starting the threads\n");
+	struct thread_args args[PTHREAD_N];
 
-	int matches = 0;
-	int found = 0;
+	for (size_t i = 0; i < PTHREAD_N; i++) {
+			args[i].tid = i;
+			args[i].user_list = head;
+			args[i].hash_table = pwd_hashes_250;
+			int success = pthread_create(&threads[i], NULL, crack_partition, &args[i]);
+			if (success != 0)
+			{
+				printf("pthread_create fail\n");
+				exit(EXIT_FAILURE);
+			}
+	}
+	join_threads();
+	printf("the end\n");
+	return 0;
+
+
 	printf("starting to check\n");
 
 	for(current = head; current ; current=current->next)
 	{
 		//for each user check top250
 		//printf("working on %s, %s\n", current->username, current->full_name);
-		found = 0;
-		//for (int i = 0; i < 10022; i++)
-		for (int i = 0; i < 261490; i++)
+		if (current->found == 0)
 		{
-				if(strcmp(current->hash, (pwd_hashes_250[i]).hash) == 0)
-				{
-					printf("%s:%s\n", current->username, (pwd_hashes_250[i]).pwd);
-					matches++;
-					found = 1;
-					break;
-				}
-		}
-		if (found == 0)
-		{
-			char *success = check_name_patterns(current->full_name, current->hash, salt);
-			if (success != NULL)
+			//for (int i = 0; i < 10022; i++)
+			for (int i = 0; i < 261490; i++)
 			{
-				printf("%s:%s\n", current->username, success);
-				matches++;
+					if(strcmp(current->hash, (pwd_hashes_250[i]).hash) == 0)
+					{
+						printf("%s:%s\n", current->username, (pwd_hashes_250[i]).pwd);
+						matches++;
+						break;
+					}
 			}
 		}
-
   }
 	printf("found %d\n", matches);
 	//exit(EXIT_SUCCESS);
 
 	exit(EXIT_SUCCESS);
-
-	/*
-	//while (len != -1)
-	while (lineptr1 != NULL)
-	{
-		//char *lineptr = NULL;
-		//size_t n = 0;
-		//len = getline(&lineptr, &n, shd);
-
-		lineptr1 = fgets(lineptr1, 40, shd);
-
-		if (lineptr1 != NULL)
-		{
-			char * user = malloc(6*sizeof(char));
-			if (user == NULL)
-			{
-				printf("error allocating user\n");
-				exit(EXIT_FAILURE);
-			}
-
-			char * hash = malloc(32*sizeof(char));
-			if (hash == NULL)
-			{
-				printf("error allocating user\n");
-				exit(EXIT_FAILURE);
-			}
-
-			user = memcpy(user, &lineptr1[0], 6);
-			hash = memcpy(hash, &lineptr1[7], 28);
-			//printf("%s %s\n", user, hash);
-
-			//printf("starting to check\n");
-			for (int i = 0; i < 478; i++)
-			{
-				if(strcmp(hash, (pwd_hashes_250[i]).hash) == 0)
-				{
-					printf("%s:%s\n", user, (pwd_hashes_250[i]).pwd);
-					matches++;
-				}
-
-				else
-				{
-					char *nameyear = test_name_year(pwd_hashes_250[i].pwd, salt, hash);
-					if (nameyear != NULL)
-					{
-						printf("%s:%s\n", user, nameyear);
-						matches++;
-					}
-				}
-
-				//qprintf("%s %s\n", (pwd_hashes_250[i]).hash, (pwd_hashes_250[i]).pwd);
-			}
-		}
-	}
-	printf("found %d\n", matches);
-	exit(EXIT_SUCCESS);*/
 }
 
-void parse_top_250(char * salt, struct pwd_hash ** array)
-{
-	//open file
-	FILE * top250 = fopen("top.txt", "r");
-	if (top250 == NULL)
-	{
-		printf("error opening top250\n");
-		exit(EXIT_FAILURE);
-	}
-
-	//passwords are the last element of the line
-	//ssize_t len = 0;
-	int index = 0;
-	//struct pwd_hash * array = malloc(250 * sizeof(struct pwd_hash));
-	char *pwd = malloc(32 * sizeof(char));
-	while (pwd != NULL)
-	{
-		pwd = fgets(pwd, 32, top250);
-		if (pwd != NULL)
-		{
-			char * token = strtok(pwd, "\n");
-			//printf("%s - %s\n", token, salt);
-			//char *hash = crypt(token, salt);
-			//printf("hash %s\n", hash);
-
-			(*array)[index].pwd = malloc(32 * sizeof(char));
-			(*array)[index].pwd = strcpy((*array)[index].pwd, token);
-			//(*array)[index].pwd = token;
-			(*array)[index].hash = malloc(32 * sizeof(char));
-			char * hash = do_pwd_hash(token, salt);
-			(*array)[index].hash = strcpy((*array)[index].hash, hash);
-		}
-//		free(pwd);
-//		char *pwd = malloc(32 * sizeof(char));
-	}
-	/*
-	while(len >= 0)
-	{
-		char *lineptr = NULL;
-		size_t n = 0;
-		len = getline(&lineptr, &n, top250);
-		//printf("%s", lineptr);
-
-		//get the password from the line
-		char * token = NULL;
-		if (len > 0)
-		{
-
-			token = strtok(lineptr, "\t");
-			token = strtok(NULL, "\t");
-			token = strtok(NULL, "\t");
-			token = strtok(NULL, "\n");
-			printf("%s\n", token);
-			//compute pwd hash and return
-
-			(*array)[index].pwd = malloc(32 * sizeof(char));
-			(*array)[index].pwd = strcpy((*array)[index].pwd, token);
-			//(*array)[index].pwd = token;
-			(*array)[index].hash = malloc(32 * sizeof(char));
-			char * hash = do_pwd_hash(token, salt);
-			(*array)[index].hash = strcpy((*array)[index].hash, hash);
-
-		}
-
-
-		index++;
-		free(lineptr);
-	}*/
-
-}
 
 char * do_pwd_hash(char * pwd, char * salt)
 {
@@ -420,4 +316,43 @@ char * test_name_year(char *name, char *salt, char *hash)
 		}
 	}
 	return NULL;
+}
+
+
+void * crack_partition(void * arg)
+{
+	struct thread_args a = *(struct thread_args *) arg;
+
+	struct user_details * head = a.user_list;
+	struct pwd_hash * table = a.hash_table;
+	struct user_details * current;
+
+	for(current = head; current ; current=current->next)
+	{
+		//for each user that is still missing, check all hash table entries
+		if ((current->found == 0) && (current->uid == a.tid))
+		{
+			for (int i = 0; i < 261490; i++)
+			{
+					if(strcmp(current->hash, (table[i]).hash) == 0)
+					{
+						//use mutex and fflush buffer
+						printf("%s:%s\n", current->username, (table[i]).pwd);
+						break;
+					}
+			}
+		}
+
+  }
+	return NULL;
+}
+
+void join_threads(void)
+{
+    for (size_t i = 0; i < PTHREAD_N; i++)
+        if (pthread_join(threads[i], NULL) != 0)
+				{
+            printf("pthread_join failed\n");
+						exit(EXIT_FAILURE);
+				}
 }
